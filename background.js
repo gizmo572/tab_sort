691a7f0a76901b492ac67ef9684b7ba198598aa7
Nick Cushman <cushmann572@gmail.com>
2024-02-06 22:00:56 -0800

    feat: add functionality to arrange all chrome browser windows in a tiled fashion in the primary browser window. popup.js: send message to background.js process on click of new 'arrange windows' button added to popup. background.js: add function to calculate dimensions of primary monitor on-start of extension. add 'arrangeWindows' listener, and algorithm for tiling up to 6 browser windows. TODO: add support for tiling windows across multiple monitors.

--- background.js
+++ background.js
@@ -1,4 +1,14 @@
+function onStart() {
+  chrome.system.display.getInfo(async (displays) => {
+    console.log(displays[0].bounds.height)
+    displayWidth = await displays[0].bounds.width;
+    displayHeight = await displays[0].bounds.height;
+  });  
+};
 
+let displayWidth, displayHeight;
+
+onStart();
 
 chrome.runtime.onMessage.addListener((request) => {
 
@@ -17,8 +27,52 @@ chrome.runtime.onMessage.addListener((request) => {
           chrome.tabs.move(tab.id, { index: idx });
         });
       })
+    });
+  };
+
+  if (request === 'arrangeWindows') {
+    console.log(displayHeight, displayWidth)
 
 
+    chrome.windows.getAll({ populate: true }, (windows) => {
+      let currentLeft = 0;
+      let windowWidth;
+
+      if (windows.length <= 3) {
+        windowWidth = displayWidth / windows.length;
+
+        windows.forEach(window => {
+          chrome.windows.update(window.id, {
+            left: currentLeft,
+            top: 0,
+            width: windowWidth,
+            height: displayHeight,
+          });
+          currentLeft += windowWidth;
+        });
+
+      } else if (windows.length <= 6) {
+        let rowCount = Math.ceil(windows.length / 2);
+        let windowTop = 0;
+
+        windowWidth = displayWidth / rowCount;
+
+        windows.forEach((window, idx) => {
+          if (idx === rowCount) {
+            currentLeft = 0;
+            windowWidth = displayWidth / Math.floor(windows.length / 2);
+            windowTop = displayHeight / 2 + 10;
+          };
+
+          chrome.windows.update(window.id, {
+            left: currentLeft,
+            top: windowTop,
+            width: windowWidth,
+            height: displayHeight / 2 - 10,
+          });
+          currentLeft += windowWidth;
+        });
+      };
     });
   };
 });
--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
 
 document.addEventListener('DOMContentLoaded', () => {
   const sortTabsBTN = document.querySelector('button#sort_tabs');
+  const arrangeWindowsBTN = document.querySelector('button#arrange_windows');
+
   sortTabsBTN.addEventListener('click', () => {
-    chrome.runtime.sendMessage('sortTabs')
-  })
+    chrome.runtime.sendMessage('sortTabs');
+  });
+  arrangeWindowsBTN.addEventListener('click', () => {
+    chrome.runtime.sendMessage('arrangeWindows');
+  });
 });
