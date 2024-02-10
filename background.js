d51f651c2a82d9c25541eea04fe11768c408d2ae
Nick Cushman <cushmann572@gmail.com>
2024-02-09 19:43:43 -0800

    feat: adjust arrange windows feature to arrange windows across all available monitors. background.js: create algorithm for setting the display to-be-used based on the ratio of windows to displays and the number of windows left to be placed, adjust the 'left' and 'width' values accordingly so that there is no wasted monitor space, set 'focused' value for all windows to 'true' so they will overtake any other open programs.

--- background.js
+++ background.js
@@ -38,48 +38,36 @@ chrome.runtime.onMessage.addListener((request) => {
   };
 
   if (request === 'arrangeWindows') {
-    console.log(displayHeight, displayWidth)
-
 
     chrome.windows.getAll({ populate: true }, (windows) => {
-      let currentLeft = 0;
-      let windowWidth;
+      console.log('windows', windows)
+      let ratio = windows.length / displaysArray.length;
+      console.log('ratio', ratio)
 
-      if (windows.length <= 3) {
-        windowWidth = displayWidth / windows.length;
+        // 1 or 2 windows per monitor
+        let displayIdx = -1, left, width;
+        for (let i = 0; i < windows.length; i++) {
 
-        windows.forEach(window => {
-          chrome.windows.update(window.id, {
-            left: currentLeft,
-            top: 0,
-            width: windowWidth,
-            height: displayHeight,
+          if (windows.length - i < displaysArray.length - displayIdx) {
+            displayIdx++;
+            width = displaysArray[displayIdx].bounds.width;
+            left = displaysArray[displayIdx].bounds.left;
+          } else if (i % 2 === 0) {
+            displayIdx++;
+            width = displaysArray[displayIdx].bounds.width / 2;
+            left = displaysArray[displayIdx].bounds.left;
+          } else {
+            width = displaysArray[displayIdx].bounds.width / 2;
+            left = displaysArray[displayIdx].bounds.left + width;
+          }
+          chrome.windows.update(windows[i].id, {
+            left: left,
+            top: displaysArray[displayIdx].bounds.top,
+            width: width,
+            height: displaysArray[displayIdx].bounds.height,
+            focused: true,
           });
-          currentLeft += windowWidth;
-        });
-
-      } else if (windows.length <= 6) {
-        let rowCount = Math.ceil(windows.length / 2);
-        let windowTop = 0;
-
-        windowWidth = displayWidth / rowCount;
-
-        windows.forEach((window, idx) => {
-          if (idx === rowCount) {
-            currentLeft = 0;
-            windowWidth = displayWidth / Math.floor(windows.length / 2);
-            windowTop = displayHeight / 2 + 10;
-          };
-
-          chrome.windows.update(window.id, {
-            left: currentLeft,
-            top: windowTop,
-            width: windowWidth,
-            height: displayHeight / 2 - 10,
-          });
-          currentLeft += windowWidth;
-        });
-      };
+        };
     });
   };
   if (request === 'cycleWindows') {
